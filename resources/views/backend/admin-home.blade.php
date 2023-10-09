248e7392e94859c6852c686cb21ff2c216ef9324
divyeshsarvaiya <divyeshjainam@gmail.com>
2023-10-09 11:20:59 +0530

    Pahse one demo

--- resources/views/backend/admin-home.blade.php
+++ resources/views/backend/admin-home.blade.php
@@ -13,22 +13,22 @@
                             <div class="table-responsive">
                                 <table class="table table-striped">
                                     <thead>
-                                        <th>{{ __('Order ID') }}</th>
-                                        <th>{{ __('Package Name') }}</th>
-                                        <th>{{ __('Payment Status') }}</th>
+                                        <th>{{ __('Api Sr') }}</th>
+                                        <th>{{ __('API Title') }}</th>
+                                        <th>{{ __('API Type') }}</th>
                                         <th>{{ __('Date') }}</th>
                                     </thead>
                                     <tbody>
-                                        @foreach ($package_recent_order as $data)
+                                        @php $sr = 0; @endphp
+                                        @foreach ($apiList as $key => $data)
                                             <tr>
-                                                <td>#{{ $data->id }}</td>
-                                                <td>{{ $data->package_name }}</td>
+                                                <td>#{{ ++$sr }}</td>
+                                                <td>{{ $data->api_title }}</td>
                                                 <td>
-                                                    @php $pay_status = $data->payment_status; @endphp
-                                                    @if ($pay_status == 'complete')
-                                                        <span class="alert alert-success">{{ __($pay_status) }}</span>
-                                                    @elseif($pay_status == 'pending')
-                                                        <span class="alert alert-warning">{{ __($pay_status) }}</span>
+                                                    @if ($data->api_type == '1')
+                                                        <span class="alert alert-success">{{ __('PAID') }}</span>
+                                                    @elseif($data->api_type == '0')
+                                                        <span class="alert alert-warning">{{ __('FREE') }}</span>
                                                     @endif
                                                 </td>
                                                 <td>{{ date_format($data->created_at, 'd M Y h:i:s') }}</td>
@@ -47,22 +47,81 @@
                             <div class="table-responsive">
                                 <table class="table table-striped">
                                     <thead>
+                                        <th>{{ __('User ID') }}</th>
+                                        <th>{{ __('User Name') }}</th>
+                                        <th>{{ __('User Details') }}</th>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>#01</td>
+                                            <td>ABC PQR</td>
+                                            <td>
+                                                <span>8460861111</span><br>
+                                                <span>pqr@gmail.com</span>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>#03</td>
+                                            <td>XYZ DEF</td>
+                                            <td>
+                                                <span>9990861417</span><br>
+                                                <span>def@gmail.com</span>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>#03</td>
+                                            <td>Divyesh Sarvaiya</td>
+                                            <td>
+                                                <span>8460861417</span><br>
+                                                <span>abc@gmail.com</span>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="card margin-top-40">
+                        <div class="smart-card">
+                            <h4 class="title">{{ __('New Leads') }}</h4>
+                            <div class="table-responsive">
+                                <table class="table table-striped">
+                                    <thead>
                                         <th>{{ __('Order ID') }}</th>
-                                        <th>{{ __('Package Name') }}</th>
+                                        <th>{{ __('Lead Info') }}</th>
+                                        <th>{{ __('Contact Details') }}</th>
                                         <th>{{ __('Payment Status') }}</th>
                                         <th>{{ __('Date') }}</th>
                                     </thead>
                                     <tbody>
-                                        @foreach ($package_recent_order as $data)
+                                        @php $sr = 0; @endphp
+                                        @foreach ($Leads as $key => $data)
                                             <tr>
-                                                <td>#{{ $data->id }}</td>
-                                                <td>{{ $data->package_name }}</td>
+                                                <td>#{{ ++$sr }}</td>
                                                 <td>
-                                                    @php $pay_status = $data->payment_status; @endphp
-                                                    @if ($pay_status == 'complete')
-                                                        <span class="alert alert-success">{{ __($pay_status) }}</span>
-                                                    @elseif($pay_status == 'pending')
-                                                        <span class="alert alert-warning">{{ __($pay_status) }}</span>
+                                                    {{ $data->lead_name }}<br>
+                                                    {{ $data->lead_occupation }}
+                                                </td>
+                                                <td>
+                                                    {{ $data->lead_email }}<br>
+                                                    {{ $data->lead_mobile }}
+                                                </td>
+                                                <td>
+                                                    {{ $data->lead_intrest }}
+                                                </td>
+                                                <td>
+                                                    @if ($data->lead_meta_status == '0')
+                                                            <div class="btn btn-outline-primary">{{ __('Pending') }}</div>
+                                                        @elseif($data->lead_meta_status == '1')
+                                                            <div class="btn btn-outline-warning">{{ __('InProgress') }}</div>
+                                                        @elseif($data->lead_meta_status == '2')
+                                                            <div class="btn btn-outline-success">{{ __('Complete') }}</div>
+                                                        @elseif($data->lead_meta_status == '3')
+                                                            <div class="btn btn-outline-light">{{ __('CancelED') }}</div>
+                                                        @elseif($data->lead_meta_status == '4')
+                                                            <div class="btn btn-outline-danger">{{ __('Deleted') }}</div>
                                                     @endif
                                                 </td>
                                                 <td>{{ date_format($data->created_at, 'd M Y h:i:s') }}</td>
@@ -180,40 +239,6 @@
                     <div class="card margin-top-40">
                         <div class="smart-card">
                             <h4 class="title">{{ __('Near Expiry Users') }}</h4>
-                            <div class="table-responsive">
-                                <table class="table table-striped">
-                                    <thead>
-                                        <th>{{ __('Order ID') }}</th>
-                                        <th>{{ __('Package Name') }}</th>
-                                        <th>{{ __('Payment Status') }}</th>
-                                        <th>{{ __('Date') }}</th>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($package_recent_order as $data)
-                                            <tr>
-                                                <td>#{{ $data->id }}</td>
-                                                <td>{{ $data->package_name }}</td>
-                                                <td>
-                                                    @php $pay_status = $data->payment_status; @endphp
-                                                    @if ($pay_status == 'complete')
-                                                        <span class="alert alert-success">{{ __($pay_status) }}</span>
-                                                    @elseif($pay_status == 'pending')
-                                                        <span class="alert alert-warning">{{ __($pay_status) }}</span>
-                                                    @endif
-                                                </td>
-                                                <td>{{ date_format($data->created_at, 'd M Y h:i:s') }}</td>
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-12">
-                    <div class="card margin-top-40">
-                        <div class="smart-card">
-                            <h4 class="title">{{ __('New Leads') }}</h4>
                             <div class="table-responsive">
                                 <table class="table table-striped">
                                     <thead>
--- resources/views/frontend/code-page.blade.php
+++ resources/views/frontend/code-page.blade.php
@@ -13,9 +13,9 @@
             to right,
             #FFFFFF 0%,
             #FFFFFF 58%,
-            #000000 58%,
-            #000000 59%,
-            #263238 59%,
+            #263238 58%,
+            /* #263238 59%,
+            #263238 59%, */
             #263238 100%
         );
     }
@@ -55,7 +55,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-2 bg-nav-section-color">
-                    <nav class="align-items-baseline bg-light h-100 navbar position-fixed pt-lg-5">
+                    <nav class="align-items-baseline bg-light h-100 navbar position-fixed w-100">
                         <ul class="navbar-nav w-100">
                             <li class="nav-item">
                                 <a class="nav-link" href="#overview">Overview</a>
@@ -90,7 +90,7 @@
                         </div>
                     </div>
                     @foreach ($api_meta_list as $meta_key => $meta_details)
-                        <div class="row py-2" id="{{$meta_details->api_meta_slug}}">
+                        <div class="row py-2 border-top" id="{{$meta_details->api_meta_slug}}">
                             <div class="col-lg-7">
                                 <div class="heading mb-3">
                                     <h1 class="sc-htoDjs WxWXp fs-24">
@@ -117,113 +117,65 @@
             </div>
         </div>
     </section>
-    <section class="" id="package">
-        <div class="container-fluid"> 
-            <div class="row">
-                <div class="col-lg-2 bg-nav-section-color">
-                </div>
-                <div class="col-lg-10 py-5">
-                    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-                        <h1 class="display-4">Pricing</h1>
-                        <p class="lead">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It's built with default Bootstrap components and utilities with little customization.</p>
+    @if ($api_details->api_type == 1)
+        <section class="" id="package">
+            <div class="container-fluid"> 
+                <div class="row">
+                    <div class="col-lg-2 bg-nav-section-color">
                     </div>
-                    {{-- <div class="container"> --}}
-                        <div class="owl-carousel">
-                            @foreach ($all_package as $key => $data)
-                                <div class="item text-center h-100">
-                                    <div class="card mb-2 box-shadow h-100">
-                                        <div class="card-header">
-                                            <h4 class="my-0 font-weight-normal">{{ $data->api_plan_title }}</h4>
-                                        </div>
-                                        <div class="card-body">
-                                            @if ($data->api_plan_discounted_price > 0)
-                                                <div class="card-title pricing-card-title fs-24 text-dark fw-900">{{ amount_with_currency_symbol($data->api_plan_discounted_price) }} <small class="text-muted text-dark fw-900">/ mo</small></div>
-                                                <div class="card-title pricing-card-title fs-18"><del>{{ amount_with_currency_symbol($data->api_plan_regular_price) }} <small class="text-muted">/ mo</small></del></div>
-                                                @else
-                                                <div class="card-title pricing-card-title fs-24 text-dark fw-900">{{ amount_with_currency_symbol($data->api_plan_regular_price) }} <small class="text-muted text-dark fw-900">/ mo</small></div>
-                                                <div class="card-title pricing-card-title fs-18" style="color: transparent">0</div>
-                                            @endif
-                                            <div class="alert alert-success justify-content-center fw-800" role="alert">
-                                                @if ($data->api_plan_discounted_price > 0)
-                                                    {{ $data->api_discounted_off_text }}
-                                                @else
-                                                    {{ 'Hurry Up Now' }}
-                                                @endif
+                    <div class="col-lg-10 py-5">
+                        <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+                            <h1 class="display-4">Pricing</h1>
+                            <p class="lead">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It's built with default Bootstrap components and utilities with little customization.</p>
+                        </div>
+                        {{-- <div class="container"> --}}
+                            <div class="owl-carousel">
+                                @foreach ($all_package as $key => $data)
+                                    <div class="item text-center h-100">
+                                        <div class="card mb-2 box-shadow h-100">
+                                            <div class="card-header">
+                                                <h4 class="my-0 font-weight-normal">{{ $data->api_plan_title }}</h4>
                                             </div>
-                                            <ul class="list-unstyled mt-3 mb-4">
-                                                @php
-                                                    $features = explode(",", $data->api_plan_descripetion);
-                                                @endphp
-                                                @foreach ($features as $item)
-                                                    <li>{{ $item }}</li>
-                                                @endforeach
-                                            </ul>
+                                            <div class="card-body">
+                                                @if ($data->api_plan_discounted_price > 0)
+                                                    <div class="card-title pricing-card-title fs-24 text-dark fw-900">{{ amount_with_currency_symbol($data->api_plan_discounted_price) }} <small class="text-muted text-dark fw-900">/ mo</small></div>
+                                                    <div class="card-title pricing-card-title fs-18"><del>{{ amount_with_currency_symbol($data->api_plan_regular_price) }} <small class="text-muted">/ mo</small></del></div>
+                                                    @else
+                                                    <div class="card-title pricing-card-title fs-24 text-dark fw-900">{{ amount_with_currency_symbol($data->api_plan_regular_price) }} <small class="text-muted text-dark fw-900">/ mo</small></div>
+                                                    <div class="card-title pricing-card-title fs-18" style="color: transparent">0</div>
+                                                @endif
+                                                <div class="alert alert-success justify-content-center fw-800" role="alert">
+                                                    @if ($data->api_plan_discounted_price > 0)
+                                                        {{ $data->api_discounted_off_text }}
+                                                    @else
+                                                        {{ 'Hurry Up Now' }}
+                                                    @endif
+                                                </div>
+                                                <ul class="list-unstyled mt-3 mb-4">
+                                                    @php
+                                                        $features = explode(",", $data->api_plan_descripetion);
+                                                    @endphp
+                                                    @foreach ($features as $item)
+                                                        <li>{{ $item }}</li>
+                                                    @endforeach
+                                                </ul>
 
-                                        </div>
-                                        <div class="card-footer">
-                                            <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+                                            </div>
+                                            <div class="card-footer">
+                                                <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            @endforeach
-                          <!-- Add more slides as needed -->
-                        </div>
-                    {{-- </div> --}}
+                                @endforeach
+                            <!-- Add more slides as needed -->
+                            </div>
+                        {{-- </div> --}}
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+    @endif
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script>
-        // var elements = document.getElementsByTagName('div')
-        // for (var i = 0; i < elements.length; i++) {
-        //     var element = elements[i];
-
-        //     // Check if the element is empty
-        //     if (element.innerHTML.trim() === '') {
-        //         console.log('element.id :>> ', element);
-        //         // Remove the empty element
-        //         // element.parentNode.removeChild(element);
-        //     }
-        // // }
-
-        function removePaddingFromDivs(element, index) {
-            var childNodes = element.childNodes;
-            console.log('index :>> ', childNodes.length);
-            for (var i = 0; i < childNodes.length; i++) {
-                var childNode = childNodes[i];
-                // console.log('childNode :>> ', childNode);
-                if (childNode.hasOwnProperty("padding-left")) {
-                    childNode.style["padding-left"] = '';
-                }
-
-                if (childNode.hasOwnProperty("padding-right")) {
-                    childNode.style["padding-right"] = '';
-                }
-                if (childNode.hasOwnProperty("width")) {
-                    childNode.style["width"] = '100%';
-                }
-                if (childNode.hasOwnProperty("border-top")) {
-                    childNode.style["width"] = '100%';
-                }
-                try {
-                    console.log('childNodes :>> ', childNodes.innerHTML.trim() === '');
-                    if (childNodes.innerHTML.trim() === '') {
-                        childNodes.parentNode.removeChild(childNodes);
-                    }
-                    removePaddingFromDivs(childNodes, index++);
-                } catch (error) {
-                    // console.log('error :>> ', error);
-                }
-            }
-        }
-
-        var parentElements = document.getElementsByClassName("body");
-        for (var i = 0; i < parentElements.length; i++) {
-            var parentElement = parentElements[i];
-            removePaddingFromDivs(parentElement, 0);
-        }
-    </script>
 <script>
 
     $('.editors').each(function (index, element) {
@@ -236,6 +188,7 @@
         editor.setOptions({
             maxLines: Infinity
         });
+        editor.getSession().setUseWrapMode(true);
     });
 
     $('.nav-link').on('click', function(event) {
